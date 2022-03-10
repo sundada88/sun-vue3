2e2b02d27fb439e3260d6630714ecee943e31b9d
lane.sun <lane.sun@zoom.us>
2022-03-10 08:46:32 +0800

    simple reactive and effect

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,14 +1,14 @@
 import { track, trigger } from './effect'
 
-export function reactive (target) {
+export function reactive(target) {
   return new Proxy(target, {
-    get (target, key, receiver) {
+    get(target, key, receiver) {
       const res = Reflect.get(target, key, receiver)
       // TODO: track
       track(target, key)
       return res
     },
-    set (target, key, value, receiver) {
+    set(target, key, value, receiver) {
       const res = Reflect.set(target, key, value, receiver)
       // TODO: trigger
       trigger(target, key)
